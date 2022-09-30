a19b8ba9e793d98a173ba2732ec54f99bd0223e6
Mohammad Hamza Ibrahim <mi469@live.mdx.ac.uk>
2022-09-30 22:39:12 +0100

    completed product layout and button.

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -3,9 +3,9 @@ import Product from "./Product";
 function ProductFeed({products}) {
     // console.log({products})
   return (
-    <div>
-        {/* <h1>{products}</h1> */}
-    {products.map(({id,title,price,description,category,image})=>(
+    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
+        {/* row-dense means use all space avalible on screen.puts products in grid if it hits medium screen it changes the coloums to 2  -mt means negative mt*/}
+    {products.slice(0,4).map(({id,title,price,description,category,image})=>(
         <Product
         key={id}
         id={id}
@@ -16,6 +16,35 @@ function ProductFeed({products}) {
         image={image}
         />
         ))}
+
+        <img className="md:col-span-full" src="https://links.papareact.com/dyz"/>
+
+        <div className="md:col-span-2">
+        {products.slice(4,5).map(({id,title,price,description,category,image})=>(
+        <Product
+        key={id}
+        id={id}
+        title={title}
+        price={price}
+        description={description}
+        category={category}
+        image={image}
+        />
+        ))}
+        </div>
+
+        {products.slice(5,products.length).map(({id,title,price,description,category,image})=>(
+        <Product
+        key={id}
+        id={id}
+        title={title}
+        price={price}
+        description={description}
+        category={category}
+        image={image}
+        />
+        ))}
+
     </div>
   );
 }
